45e05acc7aaa845e9306300e0ce879c3bbd5a2e7
Hridoy <hriodykumarshil12@gmail.com>
2023-08-28 22:01:44 +0600

    Class Based API View--- Using mixins and Using generic class-based views
    https://www.django-rest-framework.org/tutorial/3-class-based-views/#tutorial-3-class-based-views
    
    https://www.django-rest-framework.org/api-guide/generic-views/
    
    The generic views provided by REST framework allow you to quickly build API views that map closely to your database models.

--- apiproject/app/urls.py
+++ apiproject/app/urls.py
@@ -4,7 +4,8 @@ from app import views
 urlpatterns = [
     # path("myapi/", views.api_list),
     # path("apidetails/<int:pk>/", views.api_detail),
-    path("myapi/", views.Blog_list.as_view()),
-    path("detail/<int:pk>/", views.apiDetail.as_view()),
-    # path("gav/", views.ContacttList.as_view()),
+    # path("myapi/", views.Blog_list.as_view()),
+    # path("detail/<int:pk>/", views.apiDetail.as_view()),
+    path("gav/", views.ContactList.as_view()),
+    path("mydetail/<int:pk>/", views.ContactDetail.as_view()),
 ]
--- apiproject/app/views.py
+++ apiproject/app/views.py
@@ -12,17 +12,56 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import Http404
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 
-# ###Pulling it all together
-# @api_view(["GET", "POST"])
-# def api_list(request):
-#     if request.method == "GET":
-#         snip = Contact.objects.all()
-#         serializer = ContactSerializer(snip, many=True)
+##The generic views provided by REST framework allow you to quickly build API views that map closely to your database models.
+##Using mixins / Using generic class-based views
+class ContactList(
+    generics.ListCreateAPIView,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class ContactDetail(
+    generics.RetrieveUpdateDestroyAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+# ###Class-based Views --Rewriting our API using class-based views
+# class Blog_list(APIView):
+#     def get(self, request, format=None):
+#         snippets = Contact.objects.all()
+#         serializer = ContactSerializer(snippets, many=True)
 #         return Response(serializer.data)
 
-#     elif request.method == "POST":
+#     def post(self, request, format=None):
 #         serializer = ContactSerializer(data=request.data)
 #         if serializer.is_valid():
 #             serializer.save()
@@ -30,65 +69,27 @@ from rest_framework.views import APIView
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(["GET", "PUT", "DELETE"])
-# def api_detail(request, pk):
-#     try:
-#         snip = Contact.objects.get(pk=pk)
-#     except Contact.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
+# class apiDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Contact.objects.get(pk=pk)
+#         except Contact.DoesNotExist:
+#             raise Http404
 
-#     if request.method == "GET":
-#         serializer = ContactSerializer(snip)
+#     def get(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = ContactSerializer(snippet)
 #         return Response(serializer.data)
 
-#     elif request.method == "PUT":
-#         serializer = ContactSerializer(snip, data=request.data)
+#     def put(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = ContactSerializer(snippet, data=request.data)
 #         if serializer.is_valid():
 #             serializer.save()
 #             return Response(serializer.data)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     elif request.method == "DELETE":
-#         snip.delete()
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-###Class-based Views --Rewriting our API using class-based views
-class Blog_list(APIView):
-    def get(self, request, format=None):
-        snippets = Contact.objects.all()
-        serializer = ContactSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ContactSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class apiDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Contact.objects.get(pk=pk)
-        except Contact.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ContactSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ContactSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
