906bae4d7378a828b8d7ab0d466aba50d88aa664
Hridoy <hriodykumarshil12@gmail.com>
2023-08-29 16:40:57 +0600

    Basic Authentication, Tokens Authentication
    https://www.youtube.com/watch?v=hugkwW64M8Q&list=PL-83IWJl8QhsWBNG0NlamL5l8GjzVTZqO&index=6
    
    https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication

--- apiproject/app/views.py
+++ apiproject/app/views.py
@@ -14,6 +14,13 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication,
+    TokenAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 
 ##The generic views provided by REST framework allow you to quickly build API views that map closely to your database models.
@@ -26,6 +33,14 @@ class ContactList(
 ):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+    ###BasicAuthentication
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    ###TokenAuthentication
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return self.list(request)
@@ -52,44 +67,3 @@ class ContactDetail(
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
-
-
-# ###Class-based Views --Rewriting our API using class-based views
-# class Blog_list(APIView):
-#     def get(self, request, format=None):
-#         snippets = Contact.objects.all()
-#         serializer = ContactSerializer(snippets, many=True)
-#         return Response(serializer.data)
-
-#     def post(self, request, format=None):
-#         serializer = ContactSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class apiDetail(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return Contact.objects.get(pk=pk)
-#         except Contact.DoesNotExist:
-#             raise Http404
-
-#     def get(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = ContactSerializer(snippet)
-#         return Response(serializer.data)
-
-#     def put(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = ContactSerializer(snippet, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def delete(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         snippet.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
